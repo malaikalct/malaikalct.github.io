3342726f498506c4e7c4f5209629727f28df88e0
Tjebbe Vlieg <tvlieg@vlieg.me>
2022-10-20 10:15:33 +0200

    Add nix flake boilerplate

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Malaika LCT";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in { devShell = pkgs.mkShell { buildInputs = with pkgs; [ ]; }; });
+
+}
