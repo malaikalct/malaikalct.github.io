324f63f3ee426450f31ad56cb50bb02d5a8286a3
Tjebbe Vlieg <tvlieg@vlieg.me>
2022-10-24 14:24:25 +0200

    Use bare site with Tranquilpeak theme

---
+++ README.md
@@ -0,0 +1,7 @@
+# Repository containing source for malaikalct.com
+
+## Reading
+
+- https://gohugo.io/hosting-and-deployment/hosting-on-github/
+- https://ilanjoselevich.com/blog/building-websites-using-nix-flakes-and-zola/
+- https://ayats.org/blog/flake-blog/
--- flake.nix
+++ flake.nix
@@ -3,13 +3,48 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    flake-utils.inputs.nixpkgs.follows = "nixpkgs";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    #hugo-theme = {
+    #  url = "github:kakawait/hugo-tranquilpeak-theme";
+    #  flake = false;
+    #};
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
-      in { devShell = pkgs.mkShell { buildInputs = with pkgs; [ ]; }; });
+      let
+        name = "malaikalct";
+        pkgs = import nixpkgs { inherit system; };
+        #themeName = ((builtins.fromTOML
+        #  (builtins.readFile "${hugo-theme}/theme.toml")).name);
+        #linkThemeScript = ''
+        #  mkdir -p themes
+        #  ln -snf "${hugo-theme}" themes/${themeName}
+        #'';
+        buildInputs = [ pkgs.hugo ];
+      in {
+        packages.${name} = pkgs.stdenv.mkDerivation rec {
+          pname = name;
+          version = "2022-10-20";
+          src = ./.;
+          nativeBuildInputs = buildInputs;
+          #configurePhase = linkThemeScript;
+          buildPhase = ''
+            ${pkgs.hugo}/bin/hugo --minify
+          '';
+          installPhase = ''
+            cp -r ./public/ $out
+          '';
+        };
 
+        defaultPackage = self.packages.${system}.${name};
+
+        devShell = pkgs.mkShell {
+          inherit buildInputs system;
+          #shellHook = linkThemeScript;
+        };
+      });
 }
